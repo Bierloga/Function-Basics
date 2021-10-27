b194903ec069799a9607e069d4ce29ec99fe79b1
Bierloga <90850242+Bierloga@users.noreply.github.com>
2021-10-27 17:06:51 +0200

    Add files via upload

---
+++ Function Basics/script.js
@@ -0,0 +1,22 @@
+const makePizza = function () {
+    console.log("Prepare dough");
+    console.log("Add tomato sauce");
+    console.log("Add cheese and other toppings");
+    console.log("Put the pizza in the oven");
+};
+
+// makePizza();
+// makePizza();
+// makePizza();
+
+const makeSushi = function () {
+    console.log("Boil Rice")
+    console.log("Add other ingredients")
+    console.log("Wrap and cut")
+};
+
+makePizza();
+makePizza();
+makePizza();
+makeSushi();
+makeSushi();
